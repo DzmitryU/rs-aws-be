091fa7ad67dfe3f81d59384c4a8eadba14502ad7
Dzmitry Ulezla <dzmitry.ulezla@mapbox.com>
2020-11-01 13:37:29 +0300

    feat: added 404 handling for not found product

--- product-service/src/utils/response.ts
+++ product-service/src/utils/response.ts
@@ -2,11 +2,21 @@ import {
     StatusCodes,
 } from 'http-status-codes';
 
-export const toSuccess = (data) => ({
-    statusCode: StatusCodes.OK,
-    body: JSON.stringify(data, null, 2),
-    headers: {
+const addHeaderCORS = (response) => {
+    response.headers = {
+        ...response.headers,
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
-    },
+    }
+
+    return response;
+}
+
+export const toSuccess = (data) => addHeaderCORS({
+    statusCode: StatusCodes.OK,
+    body: JSON.stringify(data, null, 2),
+});
+
+export const toNotFound = () => addHeaderCORS({
+    statusCode: StatusCodes.NOT_FOUND,
 });
