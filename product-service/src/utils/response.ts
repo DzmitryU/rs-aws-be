26369766a2c1d280355aafd06a3573fd2695d7e6
Dzmitry Ulezla <dzmitry.ulezla@mapbox.com>
2020-11-01 13:04:09 +0300

    fix: response formatted according to client requirements

--- product-service/src/utils/response.ts
+++ product-service/src/utils/response.ts
@@ -4,9 +4,7 @@ import {
 
 export const toSuccess = (data) => ({
     statusCode: StatusCodes.OK,
-    body: JSON.stringify({
-        data,
-    }, null, 2),
+    body: JSON.stringify(data, null, 2),
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
