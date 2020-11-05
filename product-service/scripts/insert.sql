f83e0c8a97ceb8c5fb4cae3b56ee98b52a07cb18
Dzmitry Ulezla <dzmitry.ulezla@mapbox.com>
2020-11-05 15:29:01 +0300

    feat: added scripts for db initialization

---
+++ product-service/scripts/insert.sql
@@ -0,0 +1,17 @@
+insert into products (id, description, price, title) values
+('7567ec4b-b10c-48c5-9345-fc73c48a8000','Zombie',2.4,'The last of us'),
+('7567ec4b-b10c-48c5-9345-fc73c48a8001','Zombie and Love',10,'The last of us 2'),
+('7567ec4b-b10c-48c5-9345-fc73c48a8002','New York is the best city in the world',23,'Spiderman'),
+('7567ec4b-b10c-48c5-9345-fc73c48a8003','Fight with computer viruses',15,'Horizon zero down'),
+('7567ec4b-b10c-48c5-9345-fc73c48a8004','Apocalypse. Survival in Moscow metro',23,'The metro'),
+('7567ec4b-b10c-48c5-9345-fc73348a8005','Soccer simulator',15,'Fifa 2021'),
+('7567ec4b-b10c-48c5-9445-fc73c48a8006','Magic game',23,'The Witcher');
+
+insert into stocks (product_id, count) values
+('7567ec4b-b10c-48c5-9345-fc73c48a8000', 4),
+('7567ec4b-b10c-48c5-9345-fc73c48a8001', 11),
+('7567ec4b-b10c-48c5-9345-fc73c48a8002', 7),
+('7567ec4b-b10c-48c5-9345-fc73c48a8003', 12),
+('7567ec4b-b10c-48c5-9345-fc73c48a8004', 2),
+('7567ec4b-b10c-48c5-9345-fc73c48a8005', 15),
+('7567ec4b-b10c-48c5-9345-fc73c48a8006', 7);
