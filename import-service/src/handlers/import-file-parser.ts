c7cd93f1a6c2c840bd3bdef5090f1d6c5c02b15e
Dzmitry Ulezla <dzmitry.ulezla@mapbox.com>
2020-11-14 14:48:39 +0300

    feat: delete uploaded file after processing

--- import-service/src/handlers/import-file-parser.ts
+++ import-service/src/handlers/import-file-parser.ts
@@ -1,4 +1,4 @@
-import { S3Event } from 'aws-lambda';
+import { S3Handler, S3Event } from 'aws-lambda';
 import 'source-map-support/register';
 import AWS from 'aws-sdk';
 import csv from 'csv-parser';
@@ -6,7 +6,7 @@ import csv from 'csv-parser';
 import {AWS_REGION, BUCKET} from '../constants';
 const s3 = new AWS.S3({ region: AWS_REGION });
 
-export const importFileParser: S3Event = (event: S3Event) => {
+export const importFileParser: S3Handler = (event: S3Event) => {
   console.log(event.Records);
   event.Records.forEach((record) => {
     const s3Stream = s3.getObject({
@@ -14,20 +14,27 @@ export const importFileParser: S3Event = (event: S3Event) => {
       Key: record.s3.object.key
     }).createReadStream();
 
+    const targetKey = record.s3.object.key.replace('uploaded', 'parsed');
+
     s3Stream.pipe(csv())
         .on('data', (data) => {
           console.log(data);
         })
         .on('end', async () => {
-          console.log(`Copy from ${BUCKET}/${record.s3.object.key}`);
+          console.log(`Moving from ${BUCKET}/${record.s3.object.key}`);
 
           await s3.copyObject({
             Bucket: BUCKET,
             CopySource: `${BUCKET}/${record.s3.object.key}`,
-            Key: record.s3.object.key.replace('uploaded', 'parsed')
+            Key: targetKey
           }).promise();
 
-          console.log(`Copied into ${BUCKET}/${record.s3.object.key.replace('uploaded', 'parsed')}`);
+          await s3.deleteObject({
+            Bucket: BUCKET,
+            Key: record.s3.object.key
+          }).promise();
+
+          console.log(`Moved into ${BUCKET}/${targetKey}`);
         });
 
   });
